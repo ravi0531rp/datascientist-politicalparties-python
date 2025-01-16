a8243390ae9a7895fe90062cc89a8f339a50b4b8
RAVI PRAKASH <ravi.0531.rp@gmail.com>
2025-01-17 01:00:37 +0530

    Update estimator.py

--- src/political_party_analysis/estimator.py
+++ src/political_party_analysis/estimator.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.mixture import GaussianMixture
 
 
 class DensityEstimator:
@@ -9,9 +10,25 @@ class DensityEstimator:
     space as per the previously used dimensionality reduction technique.
     """
 
-    def __init__(self, data: pd.DataFrame, dim_reducer, high_dim_feature_names):
+    def __init__(self, data: pd.DataFrame, dim_reducer, high_dim_feature_names : list, n_components : int = 3):
         self.data = data
         self.dim_reducer_model = dim_reducer.model
         self.feature_names = high_dim_feature_names
+        self.gmm_model = None
+        self.n_components = n_components
+        
 
     ##### YOUR CODE GOES HERE #####
+
+    def fit_distribution(self):
+        self.gmm_model = gaussianMixture(n_components = self.n_components)
+        self.gmm_model.fit(self.data)
+
+    def sample_distribution(self, num_samples = 10):
+        return self.gmm_model.sample(num_samples)[0]
+
+    def map_back(self, reduced_data):
+        self.dim_reducer_model.inverse_transform(reduced_data)
+
+    def predict(self, new_data):
+        return np.exp(self.gmm_model.score_samples(new_data))
