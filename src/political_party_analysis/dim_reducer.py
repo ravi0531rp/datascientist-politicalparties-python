f67a45a846608b56a99d6109ac40aa14bf0aab11
RAVI PRAKASH <ravi.0531.rp@gmail.com>
2025-01-17 00:36:14 +0530

    Update dim_reducer.py

--- src/political_party_analysis/dim_reducer.py
+++ src/political_party_analysis/dim_reducer.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+from sklearn.decomposition import PCA
+import pandas as pd
 
 class DimensionalityReducer:
     """Class to model a dimensionality reduction method for the given dataset.
@@ -10,5 +11,15 @@ class DimensionalityReducer:
         self.n_components = n_components
         self.data = data
         self.feature_columns = data.columns
+        self.model = None
 
     ##### YOUR CODE GOES HERE #####
+    def reduce_pca(self):
+        self.model = PCA(n_components = self.n_components)
+        reduced_data = self.model.fit_transform(self.data)
+        return pd.DataFrame(reduced_data, columns = [f"PC_{i+1}" for i in range(self.n_components)])
+
+    def inverse_transform(self, reduced_data):
+        return pd.DataFrame(self.model.inverse_transform(reduced_data), columns = self.feature_columns)
+
+    
